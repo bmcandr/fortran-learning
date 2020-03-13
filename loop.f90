019fe8dcfe88ad8e5229284657bed13ed96c7ea8
Brendan Mc <19274445+bmcandr@users.noreply.github.com>
2020-03-12 23:18:55 -0400

    add comments

--- loop.f90
+++ loop.f90
@@ -1,16 +1,25 @@
 program loop
+
+    ! a simple program that takes user input for i, j, and step
+    ! loops from i to j at step and prints i on each loop
+
+    ! create empty integer variables
     INTEGER :: i, j, step
     
+    ! take user input to set i
     print *, "Enter a starting point:"
     read *, i
     
+    ! take user input to set j
     print *, "Enter a ending point:"
     read *, j
     
+    ! take user input to set step
     print *, "Enter a step:"
     read *, step
-    
-    
+
+    ! loop from i to j by step
+    ! print i in each loop    
     DO WHILE (i .LE. j)
         print *, i
         i = i + step
